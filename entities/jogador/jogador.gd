a0aa3a837d9cdc5291e75016433cd6d619eac7d6
marcvnarj <marcusviniciusaraujosantos46@gmail.com>
2025-04-17 19:45:20 -0300

    debugged player and added player animations

--- entities/jogador/jogador.gd
+++ entities/jogador/jogador.gd
@@ -4,6 +4,9 @@ extends CharacterBody2D
 const SPEED = 100.0
 const JUMP_VELOCITY = -400.0
 
+var jumping := false
+
+@onready var animation := $Anim as AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -13,13 +16,21 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+		jumping = true
+	elif is_on_floor():
+		jumping = false
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("mv_left", "mv_right")
 	if direction:
 		velocity.x = direction * SPEED
+		animation.scale.x = direction
+		if !jumping:
+			animation.play("walk")
+	elif jumping:
+		animation.play("jump")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		animation.play("idle")
 
 	move_and_slide()
