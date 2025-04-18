008414b94f41b32b2784bfb9a6d5c32906deccf8
marcvnarj <marcusviniciusaraujosantos46@gmail.com>
2025-04-18 18:36:48 -0300

    Fixed player script

--- entities/jogador/jogador.gd
+++ entities/jogador/jogador.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 const SPEED: float = 100.0
 const JUMP_VELOCITY: float = -300.0
 
+var is_jumping: bool = false
 
 @onready var animation: AnimatedSprite2D = $Anim
 
@@ -11,13 +12,17 @@ const JUMP_VELOCITY: float = -300.0
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	else:
+		is_jumping = false
+		
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+		is_jumping = true
+		
+	
 	var direction:= Input.get_axis("mv_left", "mv_right")
 	if direction:
 		velocity.x = direction * SPEED
-		velocity.normalized()
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
@@ -26,10 +31,13 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 func anima() -> void:
-	if is_on_floor() and velocity.x != 0:
+	if not is_jumping and is_on_floor() and velocity.x != 0:
 		animation.play("walk")
-		animation.scale.x = velocity.x
-	elif not is_on_floor():
+		var normalized: Vector2 = velocity.normalized()
+		animation.scale.x = normalized.x
+	elif is_jumping:
 		animation.play("jump")
+	elif not is_jumping and not is_on_floor():
+		animation.play("fall")
 	else:
 		animation.play("idle")
