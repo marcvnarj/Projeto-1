ae3cd9063a3c483176f46fc8b389eb37edfd194d
marcvnarj <marcusviniciusaraujosantos46@gmail.com>
2025-04-17 22:36:17 -0300

    Player debug and correction in the player script

--- entities/jogador/jogador.gd
+++ entities/jogador/jogador.gd
@@ -1,26 +1,26 @@
 extends CharacterBody2D
 
 
-const SPEED = 100.0
-const JUMP_VELOCITY = -400.0
+const SPEED : float = 100.0
+const JUMP_VELOCITY : float = -400.0
 
-var jumping := false
+var jumping: bool = false
+
 
 @onready var animation := $Anim as AnimatedSprite2D
 
+
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
+	
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		jumping = true
 	elif is_on_floor():
 		jumping = false
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+
 	var direction := Input.get_axis("mv_left", "mv_right")
 	if direction:
 		velocity.x = direction * SPEED
