dab59953793e03ddf093d107f200182f05b6c066
marcvnarj <marcusviniciusaraujosantos46@gmail.com>
2025-04-24 20:32:57 -0300

    Map update

--- scenes/world/world.gd
+++ scenes/world/world.gd
@@ -2,13 +2,16 @@ extends Node2D
 
 
 @onready var background_audio: AudioStreamPlayer = $BackgroundAudio
-@onready var died_menu: Control = $DiedMenu
+@onready var game_over: Control = $GameOver
 
 
 func _ready() -> void:
 	Events.game_ended.connect(game_ended)
 
 
-func game_ended(won: bool) -> void:
+func game_ended(_won: bool) -> void:
 	background_audio.stop()
-	died_menu.show()
+	game_over.show()
+
+func _on_void_area_entered(_area: Area2D) -> void:
+	pass 
